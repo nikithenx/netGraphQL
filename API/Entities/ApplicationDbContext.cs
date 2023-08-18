54631ec5036dbeafc83d0f603a668b19955f5c92
Niklas Bauer <nikibau97@googlemail.com>
2023-08-18 15:42:07 +0200

    Added Delete Author to UI

--- API/Entities/ApplicationDbContext.cs
+++ API/Entities/ApplicationDbContext.cs
@@ -10,5 +10,16 @@ namespace API.Entities
 
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Author>()
+                .HasMany(p => p.Books)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
     }
 }
